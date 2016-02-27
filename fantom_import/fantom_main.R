c56beb8ab363014c9dbd56312e4d40ac45f87677
biodim <dmitry@horodetsky.com>
2016-02-27 14:04:54 -0500

    improved code readability

--- fantom_import/fantom_main.R
+++ fantom_import/fantom_main.R
@@ -1,5 +1,5 @@
 # Purpose:   Access the Fantom Database and Extract the Relevant Counts/Annotation for Requested Cells
-# Version:   0.5.1
+# Version:   0.5.2
 # Date:      2016-02-27
 # Author:    Dmitry Horodetsky
 #
@@ -11,11 +11,11 @@
 # Notes:     
 #
 # V 0.5:     has fantomImport, fantomSearch, fantomList
-# V 0.5.1    fantomDirect added
+# V 0.5.1:   fantomDirect added
+# V 0.5.2:   Code Readability Improved
 
 
-#Libraries
-
+#Libraries Install
 if (!require(iterators, quietly=TRUE)) {
   install.packages("iterators")
 }
@@ -24,11 +24,11 @@ if (!require(iterators, quietly=TRUE)) {
   install.packages("data.table")
 }
 
+#Libraries Load
 library(data.table)
 library(iterators)
 
 #Load Sample_DB
-
 fantom_samples <- read.table('Sample_DB.txt')
 
 
@@ -36,6 +36,10 @@ URL1 <- "http://fantom.gsc.riken.jp/5/tet/search/?c=0&c=1&c=4&c=5&c=6&c="
 URL2 <- "&filename=hg19.cage_peak_counts_ann_decoded.osc.txt.gz&q=&skip=0"
 
 fantomResults <- list() 
+
+####################
+#MAIN FUNCTIONS
+####################
 
 fantomImport <- function(keyword){
   #Check Whether Samples_DB is Loaded (in the working Directory)
@@ -46,38 +50,16 @@ fantomImport <- function(keyword){
   }
   
   #Clear the list
-  resetFantom()
+  .resetFantom()
+  
+  #Prepare the Input for the Main Function
+  query_results <- fantom_samples[ grep(keyword, fantom_samples$V1) , ]
+  fantom_access_numbers <- c(query_results[,3])
   
   #Main Function
-  query_results <- fantom_samples[ grep(keyword, fantom_samples$V1) , ]
+  .fantomMain(fantom_access_numbers)
   
-  fantom_access_numbers <- c(query_results[,3])
-  length_of_FANs <- length(fantom_access_numbers)
-  iterator_counter <- icount(length_of_FANs)
-  
-  message(paste(length_of_FANs, "Search Result(s) Were Found. Loading..."))
-  
-  for(i in fantom_access_numbers)
-  {
-    current_count <- nextElem(iterator_counter)
-    message((paste("Loading Results from Fantom Access Number",i,
-                   "(",current_count,"/",length_of_FANs,")","...")))
-    fantomResults[[current_count]] <<- 
-    {
-      fantom_df <- fread(
-        paste0(URL1,as.character(i),URL2),
-        sep="\t", header=TRUE, stringsAsFactors = FALSE, showProgress = FALSE)
-    }
-    message((paste("Results from Fantom Access Number",i, "Loaded!")))
-  }
-  message(paste("All results have been loaded into fantomResults")) 
-
 }
-
-#fantomDirect here
-#This code is similar to the fantomImport function
-#I've decided against combining them to avoid a  code mess
-
 
 fantomDirect <- function(fantom_access_numbers) {
   #Check Whether Samples_DB is Loaded (in the working Directory)
@@ -88,36 +70,17 @@ fantomDirect <- function(fantom_access_numbers) {
   }
   
   #Clear the list
-  resetFantom()
+  .resetFantom()
   
-  #shout out to http://stackoverflow.com/a/17009777
-  number_list <-unique(na.omit(as.numeric(unlist(strsplit(
-    unlist(fantom_access_numbers),"[^0-9]+")))))
+  #Prepare the input for the Main Function
+  user_query <- .character_to_numbers(fantom_access_numbers)
   
-  #boundary check
-  if (max(number_list) <= 895 & min(number_list) >=7) {
+  #Boundary Check
+  if (max(user_query) <= 895 & min(user_query) >=7) {
     
-    fantom_access_numbers <- c(number_list)
-    length_of_FANs <- length(fantom_access_numbers)
-    iterator_counter <- icount(length_of_FANs)
-    
-    message(paste(length_of_FANs, "Search Result(s) Were Found. Loading..."))
-    
-    for(i in fantom_access_numbers)
-    {
-      current_count <- nextElem(iterator_counter)
-      message((paste("Loading Results from Fantom Access Number",i,
-                     "(",current_count,"/",length_of_FANs,")","...")))
-      fantomResults[[current_count]] <<- 
-      {
-        fantom_df <- fread(
-          paste0(URL1,as.character(i),URL2),
-          sep="\t", header=TRUE, stringsAsFactors = FALSE, showProgress = FALSE)
-      }
-      message((paste("Results from Fantom Access Number",i, "Loaded!")))
-    }
-    message(paste("All results have been loaded into fantomResults")) 
-    
+    #Pass to Main Function
+    fantom_access_numbers <- c(user_query)
+    .fantomMain(fantom_access_numbers)
   }
   
   else{
@@ -144,10 +107,59 @@ fantomList <- function(){
   } else { stop("Sample_DB not found. Please put it in your working directory")
   }
   
-   return(fantom_samples)
+  return(fantom_samples)
 }
 
-resetFantom <- function(){
+##########################
+#INTERNAL HELPER FUNCTIONS
+##########################
+
+
+#Function to clear the fantomResults list
+.resetFantom <- function(){
   fantomResults[] <<- NULL
   
 }
+
+#Takes a user's query (ie character type) and extracts numbers from it
+.character_to_numbers <- function(user_query){
+  
+  #shout out to http://stackoverflow.com/a/17009777
+  number_list <-unique(na.omit(as.numeric(unlist(strsplit(
+    unlist(user_query),"[^0-9]+")))))
+  return (number_list)
+}
+
+
+#The main "ENGINE" of the fantom Import system. 
+#This function accesses the fantom Server and
+#retrieves the cells based on "Fantom Access Numbers"
+#Fantom Access Numbers are simply "columns", which
+#correspond to cells in the Fantom Data Base
+
+#Most other functions just prepare the input for this function
+
+.fantomMain <- function(fantom_access_numbers) {
+  length_of_FANs <- length(fantom_access_numbers)
+  iterator_counter <- icount(length_of_FANs)
+  
+  message(paste(length_of_FANs, "Search Result(s) Were Found. Loading..."))
+  
+  for(i in fantom_access_numbers)
+  {
+    current_count <- nextElem(iterator_counter)
+    message((paste("Loading Results from Fantom Access Number",i,
+                   "(",current_count,"/",length_of_FANs,")","...")))
+    fantomResults[[current_count]] <<- 
+    {
+      fantom_df <- fread(
+        paste0(URL1,as.character(i),URL2),
+        sep="\t", header=TRUE, stringsAsFactors = FALSE, showProgress = FALSE)
+    }
+    message((paste("Results from Fantom Access Number",i, "Loaded!")))
+  }
+  message(paste("All results have been loaded into fantomResults")) 
+  
+}
+  
+  
