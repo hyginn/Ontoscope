d4599a92da8aadd26e53abf7f4564c658a836e6f
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-03-22 03:16:07 -0400

    builds lists of lists with name,id,is_a vector from OBOCollection, but slow

--- phylify/phylify.R
+++ phylify/phylify.R
@@ -1,21 +1,22 @@
 # phylify 0.0.1 pre-release
 # see: https://github.com/thejmazz/ontoscope-phylify
-# very beta,
-# but takes ndjson produced by bionode-obo and creates an igraph
-# plot is very basic atm, need to look into customizations,etc
 
 # === Packages ===
+# JSON <-> data frame
 if (!require(jsonlite, quietly=TRUE)) install.packages("jsonlite")
 
+# R bindings to igraph
 if (!require(igraph, quietly=TRUE)) install.packages("igraph")
 
+# Gene set enrichment data structures and methods
 if (!require(GSEABase, quietly=TRUE)) {
     # try http:// if https:// URLs are not supported
     source("https://bioconductor.org/biocLite.R")
     biocLite("GSEABase")
 }
 
-# web-based graphs via plotly's JavaScript graphing library
+# WebGL based graphs via plotly's JavaScript graphing library
+# Pass *all* visualization configuration in with one JSON object
 if (!require(plotly, quietly=TRUE)) install.packages("plotly")
 
 
@@ -23,6 +24,89 @@ if (!require(plotly, quietly=TRUE)) install.packages("plotly")
 # Assumes ff-phase2-140729.obo is in same directory as this script
 # see: http://svitsrv25.epfl.ch/R-doc/library/GSEABase/html/getOBOCollection.html
 fantom <- getOBOCollection("ff-phase2-140729.obo")
+
+# fantom@.subset
+# head(fantom@.kv)
+# fantom@evidenceCode
+# fantom@type
+# fantom@.stanza
+# fantom@ids
+# length(fantom@ids) # 6170
+# fantom@.subset
+# fantom@ontology
+
+# fantom@.kv is a list of stanza_ids
+# this is confusing, since it also includes keys from the header
+# (under the .__Root__ stanza_id)
+
+ROOT_STANZA_ID = ".__Root__"
+
+# Just to type less + readabilty
+kv <- fantom@.kv
+key_ids <- kv$stanza_id
+
+
+# Get header IDs
+headerIDs <- which(key_ids == ROOT_STANZA_ID)
+
+# Okay, so we got rid of the headers. Good to go?
+# Not so fast..
+
+# tail(stanza_ids, n=30L)
+# tail(fantom@.kv, n=30L)
+
+# This parser does not distinguish between [Term]s and [Typedef]s...
+
+# There are some [Typedef]s at the end.
+# We can filter these out using grep, assuming that all good [Terms]
+# look like "some capital ID, then :, then a capital ID of numbers or letters"
+# This is quite sketchy. We are relying on ID string structure to filter
+# out information which was already made explicit in the .obo file...
+# A shortcoming bionode-obo will not have :)
+# Another note on GSEABase#getOBOCollection is that we have lost value comments
+
+# stanza_ids[grep("^[A-Za-z]+:[A-Z0-9-]+$", stanza_ids, invert=TRUE)]
+# this gives us some ids such as CL_0000056 and NCBITaxon_10088
+# After some inspection, it seems as though these IDs are different from there :
+# counterparts. 
+
+# This didn't work to keep .__Root__ out of assumed <- typedefs
+# exclude_headers <- c(character(length(headerIDs)), key_ids[which(order(key_ids) != headerIDs)])
+# assumed_typedefs <- grep("^[A-Za-z]+[:_][A-Z0-9-]+$", exclude_headers, invert=TRUE)
+
+assumed_typedefs <- grep("^[A-Za-z]+[:_][A-Z0-9-]+$", key_ids, invert=TRUE)
+assumed_typedefs <- assumed_typedefs[which(assumed_typedefs != headerIDs)]
+
+# Everything else other than these should be good
+# key_ids[c(headerIDs, assumed_typedefs)]
+
+
+# So now lets get everything that should be useful.
+term_ids <- unique(key_ids[-c(headerIDs, assumed_typedefs)])
+
+for (i in head(term_ids)) {
+    print(head(kv[grep(i, kv$stanza_id),]))
+}
+
+# kv[grep("CHEBI:23367", kv$stanza_id),][kv[grep("CHEBI:23367", kv$stanza_id),]$key == "name",]$value
+# kv[grep("CHEBI:23367", kv$stanza_id),][kv[grep("CHEBI:23367", kv$stanza_id),]$key == "is_a",]$value
+
+getty <- function(id, k) {
+    chunk <- kv[grep(id, kv$stanza_id),]
+    val <- chunk[chunk$key == k,]$value
+    # print(val)
+    return(val)
+}
+
+bigger_list <- vector("list", 6)
+
+i <- 1
+for (id in head(term_ids)) {
+    little_list <- list(ID=id, name=getty(id, "name"), is_a=getty(id, "is_a"))
+    
+    bigger_list[[i]] <-  little_list
+    i <- i+1 
+}
 
 # OBOCollection comes with the slots .stanza, .subset, and .kv
 # and methods `as` to convert b/w graphNEL and OBOCollection
