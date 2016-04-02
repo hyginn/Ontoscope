9f90663a1b66e8f16f11462e26afded2d93f0c5b
Dan Litovitz <dan.litovitz@mail.utoronto.ca>
2016-04-02 18:23:04 -0400

    sped up an operation in fantomSummarize() from ~10.5 seconds on my machine to 0.01 s
    also eliminated the side effect that fantomSummarize() was previously modifying
    fantomResults by calling deleteEmpty()

--- fantom_import/fantom_main.R
+++ fantom_import/fantom_main.R
@@ -188,9 +188,6 @@ fantomSummarize <- function(threshold){
     threshold <- NA
   }
   
-  message("Filtering Relevant Results. This step takes awhile ...")
-  .deleteEmpty_old()
-  
   fantomCounts <<- list()
   
   #Prepare list
@@ -208,23 +205,21 @@ fantomSummarize <- function(threshold){
   
   fantomCounts <<- data.frame(fantomCounts)
   
+  message("Filtering Relevant Results. This step takes awhile ...")
+  fantomCounts <<- .keep_or_delete(fantomCounts, "short_description", "p@chr", keep_NOTdelete = FALSE)
+
   message("Preparing Normalized Gene Names ...")
-  
   #Shout out to RoyalTS @
   #http://stackoverflow.com/a/22656776
   fantomCounts[1] <<- apply(fantomCounts[1],2,function(x) gsub(".+@",'',x))
-  
   message ("All Genes Normalized!")
   
   message("Fixing Duplicates ...")
-  
-  #Shout out to Ben Bolker @
-  #http://stackoverflow.com/a/10180178
-  
-  fantomCounts <<- ddply(fantomCounts,"short_description",numcolwise(sum))
+  colnames(fantomCounts)[2] <<- "Count"
+  fantomCounts <<- data.table(fantomCounts)[,sum(Count),by=short_description]
+  fantomCounts <<- data.frame(fantomCounts)
   
   message("Applying Threshold ...")
-  
   #Shout out to deseq2 manual and 
   #akrun @ http://stackoverflow.com/a/30967066
   fantomCounts <<- fantomCounts[!rowSums(fantomCounts <= (threshold-1), 2:ncol(fantomCounts)),]
@@ -299,8 +294,7 @@ filterTFs <- function(){
 
 deleteEmpty <- function(identifiers){
   .loop_fantom_list(function(i){
-    fantomResults[[i]] <<- fantomResults[[i]][apply(fantomResults[[i]][, identifiers, drop = FALSE], 1, function(x){length(grep("[[:alnum:]]", x)) > 0}), ]
-    rownames(fantomResults[[i]]) <<- NULL
+    fantomResults[[i]] <<- .keep_or_delete(fantomResults[[i]], identifiers, "[[:alnum:]]", keep_NOTdelete = TRUE)
   })
 }
 
@@ -504,6 +498,11 @@ fantomProcess <- function(){
   return(vector_out[vector_out != ""])
 }
 
+.keep_or_delete <- function(dataframe, columns, regex_pattern, keep_NOTdelete){
+  dataframe <- dataframe[apply(dataframe[, columns, drop = FALSE], 1, function(x){xor(keep_NOTdelete, length(grep(regex_pattern, x)) == 0)}), ]
+  rownames(dataframe) <- NULL
+  return(dataframe)
+}
 
 
 
